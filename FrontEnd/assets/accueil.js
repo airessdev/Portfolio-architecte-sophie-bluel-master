7724a337b7c9e67a7c77f899ead1ca9e00ef2753
renaud subrini <renaud.subrini.dev@gmail.com>
2024-01-12 13:08:26 +0100

    fonction dispcat ok

--- FrontEnd/assets/accueil.js
+++ FrontEnd/assets/accueil.js
@@ -16,19 +16,52 @@ async function dispWorks() {
 
     imgElement.src = myWorks[indexWork].imageUrl;
     figcaptionElement.innerText = myWorks[indexWork].title;
-
+    figcaptionElement.id = myWorks[indexWork].categoryId;
     gallery.appendChild(figureElement);
     figureElement.appendChild(imgElement);
     figureElement.appendChild(figcaptionElement);
-    console.log(indexWork);
   }
-
-  console.log(gallery);
 }
 
 async function getCat() {
   const cat = await fetch("http://localhost:5678/api/categories");
-  console.log(await cat.json());
+  const catParse = await cat.json();
+  return catParse;
+}
+
+async function dispCat() {
+  const myCat = await getCat();
+  const projets = document.getElementById("portfolio");
+  console.log(myCat);
+  console.log(projets);
+  const containerElement = document.createElement("div");
+  containerElement.classList.add("badgeContainer");
+  projets.insertBefore(containerElement, projets.childNodes[2]);
+  const tousDivElement = document.createElement("div");
+  tousDivElement.classList.add("badge");
+  tousDivElement.classList.add("tous");
+  const tousPElement = document.createElement("p");
+  tousPElement.innerText = "Tous";
+  containerElement.appendChild(tousDivElement);
+  tousDivElement.appendChild(tousPElement);
+
+  for (let indexCat in myCat) {
+    const badgeElement = document.createElement("div");
+    const pElement = document.createElement("p");
+    badgeElement.classList.add("badge");
+    badgeElement.classList.add("gen");
+    badgeElement.id = "c" + myCat[indexCat].id;
+    pElement.innerText = myCat[indexCat].name;
+    containerElement.appendChild(badgeElement);
+    badgeElement.appendChild(pElement);
+  }
 }
 
 dispWorks();
+
+dispCat();
+function Test() {
+  let selec = document.getElementById(portfolio);
+  selec.classList.add("off");
+}
+Test();
