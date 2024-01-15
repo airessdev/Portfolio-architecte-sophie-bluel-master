72736e226be80b576816e974238ad744c24b7384
renaud subrini <renaud.subrini.dev@gmail.com>
2024-01-15 14:59:31 +0100

    refactor dispCategoryButton

--- FrontEnd/assets/accueil.js
+++ FrontEnd/assets/accueil.js
@@ -79,25 +79,26 @@ async function dispCategory() {
 
 async function dispCategoryButton() {
   const myCategory = await getCategory();
-  const projets = document.getElementById("portfolio");
-  const containerElement = document.createElement("div");
-  containerElement.classList.add("badgeContainer");
-  projets.insertBefore(containerElement, projets.childNodes[2]);
+  const containerSelect = document.querySelector(".badgeContainer");
   const allButtonElement = document.createElement("button");
   allButtonElement.id = "c0";
   allButtonElement.classList.add("badge");
   allButtonElement.classList.add("all");
   allButtonElement.innerText = "Tous";
-  containerElement.appendChild(allButtonElement);
+  containerSelect.appendChild(allButtonElement);
 
   for (let indexCategory in myCategory) {
     const badgeButtonElement = document.createElement("button");
     badgeButtonElement.classList.add("badge");
     badgeButtonElement.classList.add("gen");
-    badgeButtonElement.id = "c" + myCategory[indexCategory].id;
+    badgeButtonElement.id = myCategory[indexCategory].id;
     badgeButtonElement.setAttribute("type", "button");
     badgeButtonElement.innerText = myCategory[indexCategory].name;
-    containerElement.appendChild(badgeButtonElement);
+    containerSelect.appendChild(badgeButtonElement);
+    badgeButtonElement.addEventListener("click", () => {
+      cleanGallery();
+      dispWorksSelect(badgeButtonElement.id);
+    });
   }
   const selectIdAll = document.getElementById("c0");
   selectIdAll.addEventListener("click", () => {
@@ -105,21 +106,21 @@ async function dispCategoryButton() {
     dispWorks();
   });
 
-  const selectId = document.getElementById("c1");
-  selectId.addEventListener("click", () => {
-    cleanGallery();
-    dispWorksSelect(1);
-  });
-  const selectId2 = document.getElementById("c2");
-  selectId2.addEventListener("click", () => {
-    cleanGallery();
-    dispWorksSelect(2);
-  });
-  const selectId3 = document.getElementById("c3");
-  selectId3.addEventListener("click", () => {
-    cleanGallery();
-    dispWorksSelect(3);
-  });
+  // const selectId = document.getElementById("c1");
+  // selectId.addEventListener("click", () => {
+  //   cleanGallery();
+  //   dispWorksSelect(1);
+  // });
+  // const selectId2 = document.getElementById("c2");
+  // selectId2.addEventListener("click", () => {
+  //   cleanGallery();
+  //   dispWorksSelect(2);
+  // });
+  // const selectId3 = document.getElementById("c3");
+  // selectId3.addEventListener("click", () => {
+  //   cleanGallery();
+  //   dispWorksSelect(3);
+  // });
 }
 function cleanGallery() {
   const gallery = document.querySelector(".gallery");
