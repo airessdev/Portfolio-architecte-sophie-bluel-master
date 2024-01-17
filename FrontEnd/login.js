ffd14fc346e6dc06ad62ffa6d8845abe4f0132c5
renaud subrini <renaud.subrini.dev@gmail.com>
2024-01-17 11:58:46 +0100

    working on loginjs

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,30 +1,30 @@
 // // tuto
 
 // // Example POST method implementation:
-// async function postData(url = "", data = { email: "", password: "" }) {
-//   // Default options are marked with *
-//   const response = await fetch(url, {
-//     method: "POST", // *GET, POST, PUT, DELETE, etc.
-//     mode: "cors", // no-cors, *cors, same-origin
-//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: "same-origin", // include, *same-origin, omit
-//     headers: {
-//       "Content-Type": "application/json",
-//       // 'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     redirect: "follow", // manual, *follow, error
-//     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//     body: JSON.stringify(data), // body data type must match "Content-Type" header
-//   });
-//   return response.json(); // parses JSON response into native JavaScript objects
-// }
+async function postData(url = "", data = { email: "", password: "" }) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
-// postData("http://localhost:5678/api/users/login", {
-//   email: "sophie.bluel@test.tld",
-//   password: "S0phie",
-// }).then((data) => {
-//   console.log(data); // JSON data parsed by `data.json()` call
-// });
+postData("http://localhost:5678/api/users/login", {
+  email: "sophie.bluel@test.tld",
+  password: "S0phie",
+}).then((data) => {
+  console.log(data); // JSON data parsed by `data.json()` call
+});
 
 function recup() {
   let button = document.getElementById("login-submit");
@@ -41,4 +41,11 @@ function recup() {
   });
 }
 
-recup();
+function login() {
+  let emailPassword = recup();
+  console.log(emailPassword);
+  emailPassword = postData("http://localhost:5678/api/users/login");
+}
+
+//recup();
+//login();
